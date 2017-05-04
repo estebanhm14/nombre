9acd00120de29ec906a42aa9f4483fe2c2d2d15f
Esteban Hincapie Mejia <esteban.hm17@outlook.com>
2017-05-04 17:16:47 -0500

    version 1
    ...

---
+++ Sacar promedio con listas.py
@@ -0,0 +1,43 @@
+
+def promedio(listaNotas):
+  """
+  Entra una lista de de notas [4,3,2,1] donde
+  cada nota tenga un porcentaje del 25%
+  y tiene como salida el resultado del promedio 
+  """
+  resultado = 0
+  acumulador = 0
+
+  for x in listaNotas:
+    acumulador = acumulador + x
+
+    resultado = acumulador/len(listaNotas)
+
+  return resultado
+
+#para sacar el promedio con un porcentaje en cada nota
+def prom(notalista,porcentaje):
+  """
+  """
+  r = 0
+  a = 0
+  posicion = range(len(notalista))
+
+  for x in posicion:
+    a = notalista[x]*porcentaje[x]
+    r = r + a
+
+  return resultado 
+#==========================================================================
+
+"""def max(lista):
+  
+  Saca el maximo de una lista
+
+  resultado = 0
+  posicion = range(lend(lista))
+
+  for alguien in posicion:
+    if(resultado < lista[alguien]
+
+  return resultado """
